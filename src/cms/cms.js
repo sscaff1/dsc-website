29cb1395de20ddf7500698822bf7503394f7492a
Steven Scaffidi <sscaff1@gmail.com>
2018-08-10 08:52:04 -0500

    change name to index

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,5 @@
 import CMS from 'netlify-cms';
+import '../components/all.scss';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import BlogPostPreview from './preview-templates/BlogPostPreview';
