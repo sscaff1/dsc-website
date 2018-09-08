2d791ecda108ef001a8635d6eeec5ae8da5861b0
Steven Scaffidi <sscaff1@gmail.com>
2018-09-08 08:57:41 -0500

    enable identity

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
         stylesPath: `${__dirname}/src/components/all.sass`,
+        enableIdentityWidget: true,
       },
     },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
